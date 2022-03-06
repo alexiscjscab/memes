89e971610ff3782aa0a8ff2687056e35711e2060
alexiscjscab <alexiscjscab@gmail.com>
2022-03-06 01:07:01 -0300

    Style CSS

--- index.js
+++ index.js
@@ -12,11 +12,10 @@ const searchButton = document.getElementById('search-button');
 
 // Get the data from the API
 function init() {
-  getMemes()
-    .then(response => {
-      data = response;
-      renderMemes(data, index);
-    });
+  getMemes().then((response) => {
+    data = response;
+    renderMemes(data, index);
+  });
 }
 
 // Event listeners
@@ -24,33 +23,33 @@ nextButton.addEventListener('click', () => {
   if (index >= data.length - 2) {
     return;
   }
-  index <= data.length - 1 ? index+=2 : null
+  index <= data.length - 1 ? (index += 2) : null;
   renderMemes(data, index);
-})
+});
 
 backButton.addEventListener('click', () => {
-  index > 0 ? index -= 2 : null;
+  index > 0 ? (index -= 2) : null;
   renderMemes(data, index);
-})
+});
 
 searchButton.addEventListener('click', () => {
   searchName(search.value);
-})
+});
 
 // Search for a meme by name
-function searchName (value) {
-  const memes = data.filter(meme => meme.name.toLowerCase().includes(value.toLowerCase()));
+function searchName(value) {
+  const memes = data.filter((meme) =>
+    meme.name.toLowerCase().includes(value.toLowerCase())
+  );
   memes.length > 0 && renderMemes(memes, 0);
 }
 
-
-// Render memes on the page 
+// Render memes on the page
 function renderMemes(data, index) {
-  
-  const memes = data.slice(index, index+2);
+  const memes = data.slice(index, index + 2);
   const ulItem = document.getElementById('ul-item');
   ulItem.innerHTML = '';
-  memes.forEach(meme => {
+  memes.forEach((meme) => {
     const ul = document.createElement('ul');
     ul.classList.add('meme');
     ul.innerHTML = `
@@ -62,9 +61,7 @@ function renderMemes(data, index) {
     `;
     ulItem.appendChild(ul);
   });
-  
 }
 
-
 // Initialize the app
-init()
+init();
